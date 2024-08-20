02f5833082a0713663eb5f54c4941d7e8de7bea8
jenit-tech <nxtwave@ip-10-2-0-112.ap-south-1.compute.internal>
2024-08-20 09:59:21 +0000

    first commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,41 @@
+import CartContext from '../../context/CartContext'
+import Header from '../Header'
+import CartListView from '../CartListView'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+
+  <CartContext.Consumer>
+{value => {
+  const {cartList} = value
+  const showEmptyView = cartList.length === 0
+
+  return (
+  <>
+    <Header />
+    {showEmptyView ? (
+          <EmptyCartView/>
+        ): (
+          <div className="cart-container">
+      <div className="cart-content-container">
+        <h1 className="cart-heading">My Cart</h1>
+        <CartListView />
+      </div>
+      </div>
+        )}
+
+
+    
+  </>
+  )
+}}
+</CartContext.Consumer>
+  
+)
+
+
+
+
+export default Cart
